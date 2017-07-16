ef2c2cdb5ab75f29aa38d67ce3cfeeee95e90174
BlueNova42 <BlueNova42@users.noreply.github.com>
2017-07-16 17:59:12 -0600

    Update professions.js

--- professions.js
+++ professions.js
@@ -11,6 +11,54 @@ G.AddData({
          id: 'specialized',
          name: 'Specialized'
       });
+      new G.Res({
+         name: 'compost',
+         desc: '[compost] is decayed material used as a plant fertilizer.',
+         icon: [3, 7],
+         turnToByContext: {
+            'eat': {
+               'health': 0.01,
+               'happiness': 0.03
+            },
+            'decay': {
+               'spoiled food': 0.5
+            }
+         },
+         category: 'food',
+      });
+      new G.Unit({
+         name: 'farmer',
+         desc: '@turns [spoiled food] and [muddy water] into [compost]@uses [compost] to grow more herbs. @handles other farm tasks',
+         icon: [4, 10],
+         cost: {},
+         use: {
+            'worker': 1
+         },
+         staff: {
+            'metal tools': 1
+         },
+         upkeep: {
+            'coin': 1
+         },
+         effects: [{
+            type: 'convert',
+            from: {
+               'spoiled food': 3,
+               'mudddy water': 1
+            },
+            into: {
+               'compost': 1
+            },
+            chance: 1,
+            every: 1
+         }, ],
+         /*req: {
+            //'healing': true
+         },*/
+         category: 'debug',
+         //category: 'specialized',
+         priority: 50,
+      });
       new G.Unit({
          name: 'doctor',
          desc: '@uses [herb]s to heal the [sick] and the [wounded]<>The [doctor] has carefully studied how herbs work to cure illness.',
@@ -23,7 +71,7 @@ G.AddData({
             'metal tools': 2
          },
          upkeep: {
-            'coin': 0.4
+            'coin': 1
          },
          effects: [{
             type: 'convert',
