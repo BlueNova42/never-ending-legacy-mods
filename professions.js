1d7e46c164b5051e66a23f3426b12979b0c44cdc
BlueNova42 <BlueNova42@users.noreply.github.com>
2017-07-16 16:21:53 -0600

    Update professions.js

--- professions.js
+++ professions.js
@@ -1,16 +1,16 @@
 //doesnt work
-G.dAddData({
+G.AddData({
    name: 'professions',
    author: 'BlueNova42',
    desc: 'Adds more units (WIP)',
    engineVersion: 1,
-   //manifest: 'blueManifest.js',
+   manifest: 'blueManifest.js',
    requires: ['Default dataset*'],
    func: function() {
-      /*G.unitCategories.push({
+      G.unitCategories.push({
          id: 'specialized',
          name: 'Specialized'
-      });*/
+      });
       new G.Unit({
          name: 'doctor',
          desc: '@uses [herb]s to heal the [sick] and the [wounded]<>The [doctor] has carefully studied how herbs work to cure illness.',
